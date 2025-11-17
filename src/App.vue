b36bea225a90894f7a697b92ff5fb3b29254099b
cktolosa <clark.tolosa@adish.com.ph>
2025-11-18 07:30:52 +0800

    chore: installed shadcn

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from "./components/ui/button";
+</script>
 
 <template>
     <h1>hello world!</h1>
+    <Button> Click me! </Button>
 </template>
