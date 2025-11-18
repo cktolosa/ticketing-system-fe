56271315ee32820109b903940abfc084f16b597e
cktolosa <clark.tolosa@adish.com.ph>
2025-11-18 13:30:24 +0800

    chore: installed tanstack form

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <script setup lang="ts"></script>
 
 <template>
-    <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/sample">Sample</RouterLink>
-    </nav>
     <RouterView />
 </template>
--- src/modules/authentication/pages/login.vue
+++ src/modules/authentication/pages/login.vue
@@ -1,5 +1,76 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import { Field, FieldGroup, FieldLabel, FieldSet } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { useForm } from "@tanstack/vue-form";
+
+const form = useForm({
+    defaultValues: {
+        email: "",
+        password: "",
+    },
+    onSubmit: ({ value }) => {
+        alert(JSON.stringify(value));
+    },
+});
+</script>
 
 <template>
-    <h1>login page!</h1>
+    <main>
+        <form @submit.prevent.stop="form.handleSubmit">
+            <FieldSet>
+                <FieldGroup>
+                    <form.Field name="email">
+                        <template v-slot="{ field }">
+                            <Field>
+                                <FieldLabel :for="field.name">Email</FieldLabel>
+                                <Input
+                                    :id="field.name"
+                                    type="email"
+                                    :name="field.name"
+                                    :value="field.state.value"
+                                    @input="
+                                        (e: Event) =>
+                                            field.handleChange(
+                                                (e.target as HTMLInputElement)
+                                                    .value,
+                                            )
+                                    "
+                                    @blur="field.handleBlur"
+                                    placeholder="juandelacruz@gmail.com"
+                                />
+                            </Field>
+                        </template>
+                    </form.Field>
+                    <form.Field name="password">
+                        <template v-slot="{ field }">
+                            <Field>
+                                <FieldLabel :for="field.name">
+                                    Password
+                                </FieldLabel>
+                                <Input
+                                    :id="field.name"
+                                    type="password"
+                                    :name="field.name"
+                                    :value="field.state.value"
+                                    @input="
+                                        (e: Event) =>
+                                            field.handleChange(
+                                                (e.target as HTMLInputElement)
+                                                    .value,
+                                            )
+                                    "
+                                    @blur="field.handleBlur"
+                                    placeholder="********"
+                                />
+                            </Field>
+                        </template>
+                    </form.Field>
+                    <Field orientation="horizontal">
+                        <Button> Login </Button>
+                    </Field>
+                </FieldGroup>
+            </FieldSet>
+        </form>
+    </main>
 </template>
