e6d8977bd049333f045b019955776e306056b762
cktolosa <clark.tolosa@adish.com.ph>
2025-11-18 13:59:04 +0800

    chore: installed zod

--- src/modules/authentication/pages/login.vue
+++ src/modules/authentication/pages/login.vue
@@ -1,18 +1,39 @@
 <script setup lang="ts">
 import { Button } from "@/components/ui/button";
-import { Field, FieldGroup, FieldLabel, FieldSet } from "@/components/ui/field";
+import {
+    Field,
+    FieldError,
+    FieldGroup,
+    FieldLabel,
+    FieldSet,
+} from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { useForm } from "@tanstack/vue-form";
+import * as z from "zod";
+
+const schema = z.object({
+    email: z.email().min(1, "Email is required"),
+    password: z.string().min(1, "Password is required"),
+});
+
+const defaultValues: z.infer<typeof schema> = {
+    email: "",
+    password: "",
+};
 
 const form = useForm({
-    defaultValues: {
-        email: "",
-        password: "",
+    defaultValues,
+    validators: {
+        onChange: schema,
     },
     onSubmit: ({ value }) => {
         alert(JSON.stringify(value));
     },
 });
+
+function isInvalid(field: any) {
+    return field.state.meta.isTouched && !field.state.meta.isValid;
+}
 </script>
 
 <template>
@@ -20,51 +41,41 @@ const form = useForm({
         <form @submit.prevent.stop="form.handleSubmit">
             <FieldSet>
                 <FieldGroup>
-                    <form.Field name="email">
-                        <template v-slot="{ field }">
-                            <Field>
-                                <FieldLabel :for="field.name">Email</FieldLabel>
-                                <Input
-                                    :id="field.name"
-                                    type="email"
-                                    :name="field.name"
-                                    :value="field.state.value"
-                                    @input="
-                                        (e: Event) =>
-                                            field.handleChange(
-                                                (e.target as HTMLInputElement)
-                                                    .value,
-                                            )
-                                    "
-                                    @blur="field.handleBlur"
-                                    placeholder="juandelacruz@gmail.com"
-                                />
-                            </Field>
-                        </template>
+                    <form.Field v-slot="{ field }" name="email">
+                        <Field :aria-invalid="isInvalid(field)">
+                            <FieldLabel :for="field.name">Email</FieldLabel>
+                            <Input
+                                type="email"
+                                :id="field.name"
+                                :name="field.name"
+                                placeholder="juandelacruz@gmail.com"
+                                :model-value="field.state.value"
+                                @blur="field.handleBlur"
+                                @change="
+                                    field.handleChange($event.target.value)
+                                "
+                                :aria-invalid="isInvalid(field)"
+                            />
+                            <FieldError :errors="field.state.meta.errors" />
+                        </Field>
                     </form.Field>
-                    <form.Field name="password">
-                        <template v-slot="{ field }">
-                            <Field>
-                                <FieldLabel :for="field.name">
-                                    Password
-                                </FieldLabel>
-                                <Input
-                                    :id="field.name"
-                                    type="password"
-                                    :name="field.name"
-                                    :value="field.state.value"
-                                    @input="
-                                        (e: Event) =>
-                                            field.handleChange(
-                                                (e.target as HTMLInputElement)
-                                                    .value,
-                                            )
-                                    "
-                                    @blur="field.handleBlur"
-                                    placeholder="********"
-                                />
-                            </Field>
-                        </template>
+                    <form.Field v-slot="{ field }" name="password">
+                        <Field :aria-invalid="isInvalid(field)">
+                            <FieldLabel :for="field.name">Password</FieldLabel>
+                            <Input
+                                type="password"
+                                :id="field.name"
+                                :name="field.name"
+                                placeholder="********"
+                                :model-value="field.state.value"
+                                @blur="field.handleBlur"
+                                @change="
+                                    field.handleChange($event.target.value)
+                                "
+                                :aria-invalid="isInvalid(field)"
+                            />
+                            <FieldError :errors="field.state.meta.errors" />
+                        </Field>
                     </form.Field>
                     <Field orientation="horizontal">
                         <Button> Login </Button>
