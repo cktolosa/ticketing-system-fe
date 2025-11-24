422770bbb670c658479c4224ec23393aab15b7b3
cktolosa <clark.tolosa@adish.com.ph>
2025-11-24 17:51:04 +0300

    chore: replaced tanstack form with vee validate

--- src/modules/authentication/pages/login.vue
+++ src/modules/authentication/pages/login.vue
@@ -1,87 +1,73 @@
 <script setup lang="ts">
 import { Button } from "@/components/ui/button";
 import {
-    Field,
-    FieldError,
-    FieldGroup,
-    FieldLabel,
-    FieldSet,
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+  FieldSet,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
-import { useForm } from "@tanstack/vue-form";
 import * as z from "zod";
+import { toTypedSchema } from "@vee-validate/zod";
+import { useForm, Field as VeeField } from "vee-validate";
 
-const schema = z.object({
+const schema = toTypedSchema(
+  z.object({
     email: z.email().min(1, "Email is required"),
     password: z.string().min(1, "Password is required"),
-});
+  }),
+);
 
-const defaultValues: z.infer<typeof schema> = {
+const { handleSubmit } = useForm({
+  validationSchema: schema,
+  initialValues: {
     email: "",
     password: "",
-};
-
-const form = useForm({
-    defaultValues,
-    validators: {
-        onChange: schema,
-    },
-    onSubmit: ({ value }) => {
-        alert(JSON.stringify(value));
-    },
+  },
 });
 
-function isInvalid(field: any) {
-    return field.state.meta.isTouched && !field.state.meta.isValid;
-}
+const onSubmit = handleSubmit((data) => {
+  alert(JSON.stringify(data));
+});
 </script>
 
 <template>
-    <main>
-        <form @submit.prevent.stop="form.handleSubmit">
-            <FieldSet>
-                <FieldGroup>
-                    <form.Field v-slot="{ field }" name="email">
-                        <Field :aria-invalid="isInvalid(field)">
-                            <FieldLabel :for="field.name">Email</FieldLabel>
-                            <Input
-                                type="email"
-                                :id="field.name"
-                                :name="field.name"
-                                placeholder="juandelacruz@gmail.com"
-                                :model-value="field.state.value"
-                                @blur="field.handleBlur"
-                                @change="
-                                    field.handleChange($event.target.value)
-                                "
-                                :aria-invalid="isInvalid(field)"
-                            />
-                            <FieldError :errors="field.state.meta.errors" />
-                        </Field>
-                    </form.Field>
-                    <form.Field v-slot="{ field }" name="password">
-                        <Field :aria-invalid="isInvalid(field)">
-                            <FieldLabel :for="field.name">Password</FieldLabel>
-                            <Input
-                                type="password"
-                                :id="field.name"
-                                :name="field.name"
-                                placeholder="********"
-                                :model-value="field.state.value"
-                                @blur="field.handleBlur"
-                                @change="
-                                    field.handleChange($event.target.value)
-                                "
-                                :aria-invalid="isInvalid(field)"
-                            />
-                            <FieldError :errors="field.state.meta.errors" />
-                        </Field>
-                    </form.Field>
-                    <Field orientation="horizontal">
-                        <Button> Login </Button>
-                    </Field>
-                </FieldGroup>
-            </FieldSet>
-        </form>
-    </main>
+  <main>
+    <form @submit="onSubmit">
+      <FieldSet>
+        <FieldGroup>
+          <VeeField v-slot="{ field, errors }" name="email">
+            <Field :aria-invalid="!!errors.length">
+              <FieldLabel for="email">Email</FieldLabel>
+              <Input
+                v-bind="field"
+                id="email"
+                type="email"
+                placeholder="juandelacruz@gmail.com"
+                :aria-invalid="!!errors.length"
+              />
+              <FieldError :errors />
+            </Field>
+          </VeeField>
+          <VeeField v-slot="{ field, errors }" name="password">
+            <Field :aria-invalid="!!errors.length">
+              <FieldLabel for="password">Password</FieldLabel>
+              <Input
+                v-bind="field"
+                id="password"
+                type="password"
+                placeholder="********"
+                :aria-invalid="!!errors.length"
+              />
+              <FieldError :errors />
+            </Field>
+          </VeeField>
+          <Field orientation="horizontal">
+            <Button>Login</Button>
+          </Field>
+        </FieldGroup>
+      </FieldSet>
+    </form>
+  </main>
 </template>
