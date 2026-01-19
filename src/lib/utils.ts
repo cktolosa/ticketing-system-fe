db06b852414360985fb28ee6afe1732d70588a8e
cktolosa <clark.tolosa@adish.com.ph>
2026-01-19 18:33:25 +0300

    chore: removed traces of `pnpm`

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,23 +1,23 @@
-import type { ClassValue } from "clsx"
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-type DateTimeFormatOpts = Intl.DateTimeFormatOptions;
+import type { ClassValue } from "clsx";
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-export const formatDate = (
-  date: Date,
-  options: DateTimeFormatOpts = {
-    year: 'numeric',
-    month: 'long', 
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-    hour12: true, 
-  }
-) => {
-  return new Intl.DateTimeFormat('en-US', options).format(date); 
-}; 
+export const formatDate = (date: Date, opts: Intl.DateTimeFormat) => {
+  return new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+
+    // moving `opts` here instead of setting default values in the parameters
+    // so if we want e.g. `year`, we can overwrite `year` itself instead of
+    // affecting other opts such as `month`, `day`, etc.
+    ...opts,
+  }).format(date);
+};
